4d27dcc99cd2fe2d570b546c59039284818ab345
wutong <wutongtoby@gmail.com>
2020-04-01 16:54:44 +0800

    ver1

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ uLCD_4DGL uLCD(D1, D0, D2);
 int main()
 {
     uLCD.printf("107061220");
-    uLCD.line(1, 1, 1, 2, WHITE);
+    uLCD.line(1, 1, 1, 20, WHITE);
+    uLCD.line(1, 20, 20, 20, WHITE);
+    uLCD.line(20, 20, 20, 1, WHITE);
+    uLCD.line(20, 1, 1, 1, WHITE);
 }
