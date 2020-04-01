fe6a1d34a24e6ab2190359f84ee43c982665d1ea
wutong <wutongtoby@gmail.com>
2020-04-01 16:58:31 +0800

    sdfsdf

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 uLCD_4DGL uLCD(D1, D0, D2);
 int main()
 {
+    uLCD.line(1, 40, 1, 60, WHITE);
+    uLCD.line(1, 60, 20, 60, WHITE);
+    uLCD.line(20, 60, 20, 40, WHITE);
+    uLCD.line(20, 40, 1, 40, WHITE);
     uLCD.printf("107061220");
-    uLCD.line(1, 1, 1, 20, WHITE);
-    uLCD.line(1, 20, 20, 20, WHITE);
-    uLCD.line(20, 20, 20, 1, WHITE);
-    uLCD.line(20, 1, 1, 1, WHITE);
 }
