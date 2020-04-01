bade3d6952c2d887ae74da92a54aa71c34d68156
wutong <wutongtoby@gmail.com>
2020-04-01 17:46:36 +0800

    aaa

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Serial pc( USBTX, USBRX );
 DigitalIn Sample(D7);
 Thread thread;
 
-int i;
+int i = 0;
 float ADCdata[128];
 
 
@@ -18,6 +18,7 @@ void mission_thread() {
 
 int main()
 {
+    // write the uLCD
     uLCD.line(1, 40, 1, 60, WHITE);
     uLCD.line(1, 60, 20, 60, WHITE);
     uLCD.line(20, 60, 20, 40, WHITE);
@@ -28,28 +29,29 @@ int main()
     PWM.period(1.0/1000);      // 1000 Hz
     float duty_cycle = 0;
     bool positive = true;
+    PWM = 1.0;
 
     while (1) { 
-        if (duty_cycle == 1 ) {
+        if (duty_cycle == 1.0) {
             positive = false;
         }
         else if (duty_cycle == 0) {
             positive = true;
         }
-
         if (positive)
             duty_cycle += 0.1;
         else
-            duty_cycle -+ 0.1;
+            duty_cycle -= 0.1;
 
         PWM.write(duty_cycle);      // duty cycle, relative to period
-       
+        pc.printf("%1.3f\r\n",duty_cycle);
+
         if (i < 128) {
             ADCdata[i] = Sample;
             i++;
         }
         else if (i == 128) {
-           thread.start(mission_thread)
+           thread.start(mission_thread);
             i++;    
         } 
         wait(0.1f);
